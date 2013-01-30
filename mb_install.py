25b6bbb3bc193da16b97da8d0b17ac1abe90616b
Joe Sadusk <joseph.sadusk@makerbot.com>
2013-01-30 18:49:41 -0500

    Support for an install build using mac frameworks.  install_libs and headers create the framework structure, and compiler flags will pull in those frameworks

--- mb_install.py
+++ mb_install.py
@@ -29,15 +29,69 @@ def mb_glob(env, path):
     (head, tail) = os.path.split(path)
     return glob.glob(os.path.join(str(env.Dir(head)), tail))
 
-def mb_install_lib(env, source):
-    target = env.Install(env['MB_LIB_DIR'], source)
-    if sys.platform == 'win32':
-        target = [target, env.Install(env['MB_BIN_DIR'], source)]
-    env.Append(MB_INSTALL_TARGETS = target)
-    return target
+def mb_install_lib(env, source, name):
+    targets = []
+    if env.MBIsMac():
+        framework = os.path.join(env['MB_FRAMEWORK_DIR'], name + '.framework')
+        version_dir = os.path.join('Versions', env['MB_VERSION'])
+        libinst = env.InstallAs(os.path.join(framework, version_dir, name),
+                                source)
+        targets.append(libinst)
 
-def mb_install_headers(env, source, dest=''):
-    targets = env.rInstall(os.path.join(env['MB_INCLUDE_DIR'], dest), source)
+        #make relative symlinks between Current and the new version
+        current_dir = 'Current'
+        current_link = env.Command(os.path.join(framework, 'Versions',
+                                                current_dir),
+                                   libinst,
+                                   'cd ' + os.path.join(framework, 'Versions')
+                                   + ';ln -sf ' +
+                                   env['MB_VERSION'] + ' ' + current_dir)
+        targets.append(current_link)
+
+        #make a relative symlink for the current lib
+        targets.append(env.Command(os.path.join(framework, name),
+                                   current_link, 'cd ' + framework +
+                                   ';ln -s ' +
+                                   os.path.join('Versions', current_dir, name)
+                                             + ' ' + name))
+    
+    else:                 
+        targets.append(env.Install(env['MB_LIB_DIR'], source))
+        if env.MBIsWindows():
+            targets.append(env.Install(env['MB_BIN_DIR'], source))
+
+    env.Append(MB_INSTALL_TARGETS = targets)
+    return targets
+
+def mb_install_headers(env, source, name, dest=''):
+    targets = []
+    if env.MBIsMac():
+        framework = os.path.join(env['MB_FRAMEWORK_DIR'], name + '.framework')
+        version_dir = os.path.join('Versions', env['MB_VERSION'])
+        include_dir = os.path.join(version_dir, 'Headers')
+
+        targets = env.rInstall(os.path.join(framework, include_dir), source)
+        
+        # #make relative symlinks between Current and the new version
+        current_dir = os.path.join('Versions', 'Current')
+        # current_link = env.Command(os.path.join(framework, current_dir),
+        #                            targets, 'cd ' + framework +
+        #                            ';ln -s ' + version_dir + ' ' + current_dir)
+        # targets.append(current_link)
+
+        #make a relative symlink for the current headers
+        toplink = os.path.join(framework, 'Headers')
+        targets.append(env.Command(os.path.join(framework, toplink),
+                                   targets, 'cd ' + framework +
+                                   ';ln -s ' + os.path.join(current_dir,
+                                                            'Headers')
+                                             + ' ' + toplink))
+        
+    else:
+        targets = env.rInstall(os.path.join(env['MB_INCLUDE_DIR'], 
+                                            os.path.join(dest, 'name')),
+                               source)
+
     env.Append(MB_INSTALL_TARGETS = targets)
     return targets
 
@@ -76,6 +130,16 @@ def mb_install_system(env, source, dest):
 
 def create_install_target(env):
     env.Alias('install', env['MB_INSTALL_TARGETS'])
+
+def mb_use_devel_libs(env):
+    return ARGUMENTS.get('devel_libs', '') is not ''
+
+def mb_add_lib(env, name):
+    if env.MBIsMac() and not env.MBUseDevelLibs():
+        env.Append(CCFLAGS = ['-framework', name])
+        env.Append(LINKFLAGS = ['-framework', name])
+    else:
+        env.Append(LIBS = name)
 
 def add_devel_lib_path(env, path):
     if ARGUMENTS.get('devel_libs', '') is not '':
@@ -133,8 +197,12 @@ def set_install_paths(env):
 
     #OSX doesn't use the standard link lines
     if sys.platform == 'darwin':
-        if ARGUMENTS.get('devel_libs', '') is not '1':
-            env.Append(FRAMEWORKS = ['MakerBot'])
+        if not env.MBUseDevelLibs():
+            #add the fake root frameworks path
+            env['MB_FRAMEWORK_DIR'] = os.path.join(prefix,
+                                                   'Library/Frameworks')
+            env.Append(CCFLAGS = ['-F' + env['MB_FRAMEWORK_DIR']])
+            env.Append(LINKFLAGS = ['-F' + env['MB_FRAMEWORK_DIR']])
     else:
         env.Append(LIBPATH = [lib_dir])
         env.Append(CPPPATH = [include_dir])
@@ -162,6 +230,11 @@ def set_install_paths(env):
                        MB_RESOURCE_DIR = prefix + '/MakerWare',
                        MB_CONFIG_DIR = prefix + '/MakerWare',
                        MB_EGG_DIR = prefix + '/MakerWare/python')
+
+    #extract the build version
+    version_file = open(str(env.File('#/mb_version')))
+    env['MB_VERSION'] = version_file.readline()
+    version_file.close()
 
 _is_windows = ('win32' == sys.platform)
 _is_linux = (sys.platform.startswith('linux'))
@@ -197,11 +270,13 @@ def generate(env):
     env.AddMethod(create_install_target, 'MBCreateInstallTarget')
     env.AddMethod(add_devel_lib_path, 'MBAddDevelLibPath')
     env.AddMethod(add_devel_include_path, 'MBAddDevelIncludePath')
+    env.AddMethod(mb_add_lib, 'MBAddLib')
     env.AddMethod(mb_glob, 'MBGlob')
 
     env.AddMethod(mb_is_windows, 'MBIsWindows')
     env.AddMethod(mb_is_linux, 'MBIsLinux')
     env.AddMethod(mb_is_mac, 'MBIsMac')
+    env.AddMethod(mb_use_devel_libs, 'MBUseDevelLibs')
 
     env.MBSetDefaultPrefix()
     env.MBSetInstallPaths()
