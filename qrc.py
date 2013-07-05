68a6a0927792fdf771dcc1a7f639733c495c1889
Nicholas Bishop <nicholas.bishop@makerbot.com>
2013-07-05 10:24:43 -0400

    Move RecursiveFileGlob to its own tool

--- qrc.py
+++ qrc.py
@@ -1,23 +1,6 @@
 # Copyright 2013 MakerBot Industries
 
-import fnmatch
 import os
-
-def RecursiveFileGlob(env, root, pattern):
-    '''Recursively search in 'root' for files matching 'pattern'
-
-    Returns a list of matches of type SCons.Node.FS.File'''
-    def path_without_first_component(path):
-        return os.sep.join(path.split(os.sep)[1:])
-
-    matches = []
-    if root.startswith('#'):
-        raise Exception('Directories starting with "#" not supported yet')
-    for parent, dirnames, filenames in os.walk(os.path.join('..', root)):
-        for filename in fnmatch.filter(filenames, pattern):
-            matches.append(env.File(
-                    path_without_first_component(os.path.join(parent, filename))))
-    return matches
 
 def MBGenQRC(target, source, env):
     '''Create an XML QRC file'''
@@ -31,7 +14,6 @@ def MBGenQRC(target, source, env):
         f.write('</RCC>\n')
 
 def generate(env):
-    env.AddMethod(RecursiveFileGlob, 'RecursiveFileGlob')
     env.Append(BUILDERS = {'MBGenQRC': env.Builder(action = MBGenQRC)})
 
 def exists(env):
