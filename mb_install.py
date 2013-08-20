a733e083ac7598ece91703a787d7e7b99f51b747
Ted <Ted@Makerbot.com>
2013-08-20 14:13:58 -0400

    Automatically add aliases for the program/library targets.

--- mb_install.py
+++ mb_install.py
@@ -494,23 +494,26 @@ def mb_depends_on_conveyor_ui(env):
 def mb_program(env, target, source, *args, **kwargs):
     if env.MBIsWindows():
         program = env.MBWindowsProgram(target, source, *args, **kwargs)
-        return program
     else:
-        return env.Program(target, source, *args, **kwargs);
+        program = env.Program(target, source, *args, **kwargs);
+    env.Alias(target, program)
+    return program
 
 def mb_shared_library(env, target, source, *args, **kwargs):
     if env.MBIsWindows():
-        program = env.MBWindowsSharedLibrary(target, source, *args, **kwargs)
-        return program
+        library = env.MBWindowsSharedLibrary(target, source, *args, **kwargs)
     else:
-        return env.SharedLibrary(target, source, *args, **kwargs)
+        library = env.SharedLibrary(target, source, *args, **kwargs)
+    env.Alias(target, library)
+    return library
 
 def mb_static_library(env, target, source, *args, **kwargs):
     if env.MBIsWindows():
-        program = env.MBWindowsStaticLibrary(target, source, *args, **kwargs)
-        return program
+        library = env.MBWindowsStaticLibrary(target, source, *args, **kwargs)
     else:
-        return env.StaticLibrary(target, source, *args, **kwargs)
+        library = env.StaticLibrary(target, source, *args, **kwargs)
+    env.Alias(target, library)
+    return library
 
 def mb_explicit_moc_fixme(env, sources):
     target = []
