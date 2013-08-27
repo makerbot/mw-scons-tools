c33c20e85ebd76190f51c6050c66983d186e247f
Ted <Ted@Makerbot.com>
2013-08-27 15:26:40 -0400

    updating some strings in mb_sconstruct

--- mb_sconstruct.py
+++ mb_sconstruct.py
@@ -5,7 +5,8 @@ def common_arguments(env):
         '--no-variant',
         dest='no_variant',
         action='store_true',
-        help='Turns off the variant dir if it would be used. Mainly for use with IDEs.')
+        help='Turns off the variant dir if it would be used. Mainly for use with IDEs. '
+                'Note that for python projects and on windows this is on by default.')
 
 def mb_sconscript(env, sconscript, python_project = False):
     if python_project or env.MBIsWindows() or env.MBGetOption('no_variant'):
@@ -16,7 +17,7 @@ def mb_sconscript(env, sconscript, python_project = False):
 def generate(env):
     tool_exists = 'MB_SCONSTRUCT_TOOL_LOADED'
     if env.get(tool_exists, False):
-        print 'tool "common" being loaded multiple times'
+        print 'tool "mb_sconstruct" being loaded multiple times'
     else:
         env[tool_exists] = True
 
