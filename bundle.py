cd3c96f78bec1c2e6795e9a5cc2a5a3e5c35f6c4
Nicholas Bishop <nicholas.bishop@makerbot.com>
2013-09-20 14:18:34 -0400

    Adding quoting in the bundle tool to allow for 'MakerWare for Digitizer'

--- bundle.py
+++ bundle.py
@@ -11,27 +11,27 @@ def run(command) :
 def createBundle(target, source, env) :
 	bundleDir = str(target[0])
 	print "creating bundle at " + bundleDir
-	run("rm -rf "+bundleDir )
-	run("mkdir -p %s/Contents/Resources" % bundleDir )
-	run("mkdir -p %s/Contents/Frameworks" % bundleDir )
-	run("mkdir -p %s/Contents/MacOS" % bundleDir )
+	run("rm -rf '%s'" % bundleDir )
+	run("mkdir -p '%s/Contents/Resources'" % bundleDir )
+	run("mkdir -p '%s/Contents/Frameworks'" % bundleDir )
+	run("mkdir -p '%s/Contents/MacOS'" % bundleDir )
 	if env['BUNDLE_PLUGINS'] :
-		run("mkdir -p %s/Contents/plugins" % bundleDir )
+		run("mkdir -p '%s/Contents/plugins'" % bundleDir )
 	# add binaries
 	for bin in env.Flatten( env['BUNDLE_BINARIES'] ) :
-		run('cp %s %s/Contents/MacOS/' % (str(bin), bundleDir) )
+		run("cp '%s' '%s/Contents/MacOS/'" % (str(bin), bundleDir) )
 	for bin in env.Flatten( env['BUNDLE_PLUGINS'] ) :
-		run('cp %s %s/Contents/plugins/' % (str(bin), bundleDir) )
+		run("cp '%s' '%s/Contents/plugins/'" % (str(bin), bundleDir) )
 	# add resources
 	for resdir in env['BUNDLE_RESOURCEDIRS'] :
 		# TODO act sensitive to resdir being a scons target. now assuming a string
-		run('cp -r %s %s/Contents/Resources/' % (str(resdir), bundleDir) )
+		run("cp -r '%s' '%s/Contents/Resources/'" % (str(resdir), bundleDir) )
 	# write Info.plist -- TODO actually write it not copy it
 	plistFile = env['BUNDLE_PLIST']
-	run('cp %s %s/Contents/Info.plist' % (plistFile, bundleDir) )
+	run("cp '%s' '%s/Contents/Info.plist'" % (plistFile, bundleDir) )
 	# add icon -- TODO generate .icns file from png or svg
 	iconFile = env['BUNDLE_ICON']
-	run('cp %s %s/Contents/Resources' % (iconFile, bundleDir) )
+	run("cp '%s' '%s/Contents/Resources'" % (iconFile, bundleDir) )
 	# add dependent libraries, fixing all absolute paths
 	if 'BUNDLE_DEPENDENT_LIBS' in env:
             addDependentLibsToBundle( bundleDir )
