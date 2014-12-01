c2a7f1d0a96f52644374396da1b3b0cb5dfdb60c
Ted <Ted@Makerbot.com>
2014-12-01 15:05:25 -0500

    Adding a basic hook_settings.py (see T-R for documentation)

---
+++ hook_settings.py
@@ -0,0 +1,13 @@
+
+import hook_tools
+
+hooks = [
+    'pre-commit'
+]
+
+pre_commit_checks = [
+    hook_tools.cpplint_check,
+    hook_tools.pep8lint_check,
+    hook_tools.cmakelint_check,
+    hook_tools.jsonlint_check
+]
