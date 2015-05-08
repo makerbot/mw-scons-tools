22d186815e6741cc090ebdf455781bb57576d8ba
Chris Moore <chris.moore@makerbot.com>
2015-05-07 22:48:48 -0400

    Revert "Avoid infinite recursion when mustache is missing"
    This reverts commit 92deb61ce35b58bf0c5fc210bf69dccb2b40aa80.
    
    So that didn't work and things got way too messy when I tried to fix it

--- mustache.py
+++ mustache.py
@@ -1,13 +1,11 @@
 # Convenience wrapper for https://github.com/makerbot/mustache
 
-import imp
 import os
 import sys
 
-mustache_path = os.path.join(
-    os.path.dirname(__file__), os.pardir, 'mustache', 'mustache.py'
-)
+mustache_dir = os.path.join(os.path.dirname(__file__), os.pardir, 'mustache')
+sys.path.insert(0, mustache_dir)
 
 # Abort initialization of this mustache module and import the real one
 del sys.modules['mustache']
-imp.load_source('mustache', mustache_path)
+import mustache
