066a2e03151afec48e9a60d6eda22ecefc290c1d
Chris Moore <chris.moore@makerbot.com>
2015-06-30 17:44:08 -0400

    Add a method to compile against python3.4
    MW-5987
    http://jira.makerbot.net/browse/MW-5987
    Currently there is very little to this, since the build system should
    be ensuring that the necessary headers and libs are on paths that are
    already set up.

--- common.py
+++ common.py
@@ -309,6 +309,13 @@ def mb_depends_on_yajl(env):
         yajl = 'yajl'
     env.Append(LIBS = [yajl])
 
+def mb_depends_on_python34(env):
+    # On linux we expect headers and libs to already be installed to the
+    # system, and on OSX/Windows the Python3.4 distributable should put
+    # headers and libs on the install target.
+    # TODO: deal with debug builds
+    env.Append(LIBS='python3.4m')
+
 def _windows_boost_format(lib, debug):
     return lib + ('-vc120-mt-gd-1_56' if debug else '-vc120-mt-1_56')
 
@@ -393,6 +400,7 @@ def generate(env):
     env.AddMethod(mb_depends_on_opencv, 'MBDependsOnOpenCV')
     env.AddMethod(mb_depends_on_vtk, 'MBDependsOnVTK')
     env.AddMethod(mb_depends_on_yajl, 'MBDependsOnYajl')
+    env.AddMethod(mb_depends_on_python34, 'MBDependsOnPython34')
     env.AddMethod(mb_add_boost_libs, 'MBAddBoostLibs')
     env.AddMethod(mb_install_boost_libs, 'MBInstallBoostLibs')
 
