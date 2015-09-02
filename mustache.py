5d209d972ca622452025bfc12aa1012698605cfe
Ryan Howard <ryan.howard@makerbot.com>
2015-09-02 10:48:23 -0400

    look for mustache in install tree first
    BST-324
    http://jira.makerbot.net/browse/BST-324

--- mustache.py
+++ mustache.py
@@ -3,8 +3,12 @@
 import os
 import sys
 
-mustache_dir = os.path.join(os.path.dirname(__file__), os.pardir, 'mustache')
-sys.path.insert(0, mustache_dir)
+# append the sibling dir of mustache but we will prefer the installed version
+
+mustache_sibling_dir = os.path.join(os.path.dirname(__file__), os.pardir, 'mustache')
+sys.path.insert(0, mustache_sibling_dir)
+mustache_install_dir = os.path.join(os.path.dirname(__file__), os.pardir)
+sys.path.insert(0, mustache_install_dir)
 
 # Abort initialization of this mustache module and import the real one
 del sys.modules['mustache']
