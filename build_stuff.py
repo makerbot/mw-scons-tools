42568061b35f1b9ffb022d883246a01baaedaef4
Ted <Ted@Makerbot.com>
2015-11-06 17:31:54 -0500

    adding new build_stuff file

---
+++ build_stuff.py
@@ -0,0 +1,13 @@
+
+try:
+    import artifactory_utils
+except:
+    pass
+else:
+    dependencies = [
+        artifactory_utils.ArtifactSelector(
+            project="Toolchain-Release",
+            revision="develop",
+            debug=False,
+            stable_required=True)
+    ]
